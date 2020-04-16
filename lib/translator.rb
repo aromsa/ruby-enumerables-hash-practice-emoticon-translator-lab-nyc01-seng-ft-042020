4a1ed3708968e63876480a123a04a001c32ff645
anna bateman <annacbateman@gmail.com>
2020-04-16 14:24:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def load_library (file)
   emoticon_names
 end
 
-def get_japanese_emoticon(file)
+def get_japanese_emoticon(file, emoticon)
   emoticons = load_library(file)
   emoticon_japanese = {}
   emoticons.each do |key, emos|
