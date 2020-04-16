e43e3148fc6c455996320292cd497fc1efcaf707
anna bateman <annacbateman@gmail.com>
2020-04-16 13:53:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,16 @@ require 'pry'
 #emoticons = YAML.load_file("./lib/emoticons.yml")
 
 
-# def load_library (file)
-#   emoticons = YAML.load_file(file)
-#   emoticon_names = {}
-#   emoticons.each do |key, emos|
-#     emoticon_names[key] = {}
-#     emoticon_names[key][:english] = emos[0]
-#     emoticon_names[key][:japanese] = emos[1]
-#   end
-#   emoticon_names
-# end
+def load_library (file)
+  emoticons = YAML.load_file(file)
+  emoticon_names = {}
+  emoticons.each do |key, emos|
+    emoticon_names[key] = {}
+    emoticon_names[key][:english] = emos[0]
+    emoticon_names[key][:japanese] = emos[1]
+  end
+  emoticon_names
+end
 
 def get_japanese_emoticon (file)
   emoticons = YAML.load_file(file)
