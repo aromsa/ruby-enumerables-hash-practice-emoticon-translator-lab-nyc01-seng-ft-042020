2a1d0d3b7b0267f664e8b36e5ca0eb8e28e18875
anna bateman <annacbateman@gmail.com>
2020-04-16 13:32:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library (file)
     emoticon_names[key] = {}
     emoticon_names[key][:english] = emos[0]
     emoticon_names[key][:japanese] = emos[1]
-    binding.pry
+    #binding.pry
   end
   emoticon_names
 end
