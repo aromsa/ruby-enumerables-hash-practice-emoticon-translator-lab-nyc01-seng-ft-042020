252b5d30203a7bbb97571bc9a1e9c5c23929765e
anna bateman <annacbateman@gmail.com>
2020-04-16 14:47:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_english_meaning(file, emoticon)
   emoticons = load_library(file)
-  japanese = [key][:japanese][emo]
+  #japanese = [key][:japanese][emo]
   binding.pry
   if emoticon = [:english]
     puts [:japanese]
