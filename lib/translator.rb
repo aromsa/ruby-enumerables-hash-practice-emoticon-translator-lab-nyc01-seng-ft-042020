a6c473d8afb1ba46e00eb02a33f2d09f339a967f
anna bateman <annacbateman@gmail.com>
2020-04-16 14:11:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,9 +16,9 @@ def load_library (file)
 end
 
 def get_japanese_emoticon(data)
-  emoticons = YAML.load_library(data)
+  # emoticons = YAML.load_library(data)
   emoticon_japanese = {}
-  emoticons.each do |key, emos|
+  data.each do |key, emos|
     emoticon_japanese[key] = {}
     emoticon_japanese[emos[1]] = emos[0]
   end
